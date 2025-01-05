292a8494d5c2a659b367fcbdff2593ca4a168485
Brianna Root <brianna.k.root@gmail.com>
2025-01-05 17:01:32 +1300

    created onClick handler in Student

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,10 +1,19 @@
 import './StudentList.css';
 import Student from './Student';
+import PropTypes from 'prop-types';
 
 const StudentList = (props) => {
     const studentComponents = props.students.map(student => {
         return (
-            <li><Student name={student.nameData} email={student.emailData}></Student></li>
+            <li key={student.id}>
+                <Student
+                    id={student.id}
+                    name={student.nameData}
+                    email={student.emailData}
+                    isPresent={student.isPresentData}
+                    onPresenceToggle={props.onStudentPresenceToggle}
+                    ></Student>
+            </li>
         );
     });
 
@@ -17,4 +26,17 @@ const StudentList = (props) => {
         </section>
     );
 };
+
+StudentList.propTypes = {
+    students: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            nameData: PropTypes.string.isRequired,
+            emailData: PropTypes.string.isRequired,
+            isPresentData: PropTypes.bool.isRequired,
+        })
+    ),
+    onStudentPresenceToggle: PropTypes.func.isRequired,
+};
+
 export default StudentList;
